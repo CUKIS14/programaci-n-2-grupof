e03e6f91d8d14cf539c2cf36e259808f437d6805
CUKIS14 <cuellandrey@gmail.com>
2025-08-27 10:38:45 -0400

    Add files via upload

---
+++ spibox.py
@@ -0,0 +1,32 @@
+#spinbox de numero del 1 al 10 para edad
+import tkinter as tk
+from tkinter import messagebox
+def mostrarEdad():
+    tk.messagebox.showinfo("Edad",f"La edad seleccionada es :{spin.get()}")
+def mostrarGenero():
+    tk.messagebox.showinfo("genero", f"el genero seleccionado es: {spin.get()}") 
+ventana= tk.Tk()
+ventana.title("ventana")
+labelEdad=tk.Label(ventana, text="edad")
+labelEdad.grid(row=0, column=0, padx=5, pady=6, sticky="w")
+
+#genero
+labelGenero=tk.Label(ventana, text="Genero")
+labelGenero.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+
+#spinboxde texto para genero
+genero=tk.Spinbox(ventana, values=("masculino","femenino", "otro"))
+genero.grid(row=1, column=10, padx=10, pady=10)
+botonGenero=tk.Button(ventana, text="obtener genero",command=mostrarGenero)
+botonGenero.grid(row=3, column=10, padx=10, pady=10)
+
+
+spin=tk.Spinbox (ventana, from_=1,to=10)
+spin.grid(row=0, column=1, padx=10, pady=10)
+
+boton=tk.Button(ventana, text="otener valor", command=mostrarEdad)
+boton.grid(row=1, column=0, padx=10, pady=10)
+
+
+
+ventana.mainloop()
