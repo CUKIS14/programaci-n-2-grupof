af627bf001f11d39617a08ca64d27f2ab0e3433a
CUKIS14 <cuellandrey@gmail.com>
2025-09-03 10:37:50 -0400

    Add files via upload

--- noteboox.py
+++ noteboox.py
@@ -1,11 +1,38 @@
 #importacuon de libreria
 import tkinter as tk
 from tkinter import ttk,messagebox
+from datetime import datetime
 
 #crear ventana principal
 ventana_principal=tk.Tk()
 ventana_principal.title("libro de paciente y doctores")
-ventana_principal.geometry("400x600")
+ventana_principal.geometry("740x510")
+
+#funcion para enmascarar fecha
+def enmascarar_fecha(texto):
+    limpio=''.join(filter(str.isdigit, texto))
+    formato_final=""
+    
+    if len(limpio)>8:
+        limpio=limpio[:8]
+    if len(limpio)>4:
+        formato_final=f"{limpio[:2]}-{limpio[2:4]}-{limpio[4:]}"
+    elif len(limpio)>2:
+        formato_final=f"{limpio[:2]}-{limpio[2:]}"
+    else:
+        formato_final=limpio
+        
+    if fechaN.get()!=formato_final:
+        fechaN.delete(0,tk.END)
+        fechaN.insert(0, formato_final)
+    if len(fechaN.get())==10:
+        fecha_actual=datetime.now().date()
+        fecha_nacimiento=datetime.strptime(fechaN.get(),"%d-%m-%Y").date()
+        edad=fecha_actual.year-fecha_nacimiento.year
+        edadVar.set(edad)
+    else:
+        edadVar.set("")
+    return True
 
 #crear contenedor notebook
 pestañas=ttk.Notebook(ventana_principal)
@@ -29,16 +56,18 @@ nombreP=tk.Entry(frame_pacientes)
 nombreP.grid(row=0, column=1, sticky="w", pady=5, padx=5)
 
 #fecha de nacimiento
-labelFechaN=tk.Label(frame_pacientes, text="fecha de Nacimiento")
-labelFechaN.grid(row=1,column=0, sticky="w", pady=5, padx=5)
-fechaN=tk.Entry(frame_pacientes)
+labelFechaN=tk.Label(frame_pacientes, text="Fecha de Nacimiento:")
+labelFechaN.grid(row=1, column=0, sticky="w", pady=5, padx=5)
+validacion_fecha=ventana_principal.register(enmascarar_fecha)
+fechaN=ttk.Entry(frame_pacientes, validate="key", validatecommand=(validacion_fecha, '%P'))
 fechaN.grid(row=1, column=1, sticky="w", pady=5, padx=5)
 
-#edad(readol)
-labelEdad=tk.Label(frame_pacientes, text="edad")
-labelEdad.grid(row=2,column=0, sticky="w", pady=5, padx=5)
-edad=tk.Entry(frame_pacientes, state="readonly")
-edad.grid(row=2, column=1, sticky="w", pady=5, padx=5)
+#edad(readoly)
+labelEdad=tk.Label(frame_pacientes, text="Edad:")
+labelEdad.grid(row=2, column=0, sticky="w", pady=5, padx=5)
+edadVar=tk.StringVar()
+edadP=tk.Entry(frame_pacientes, textvariable=edadVar,state="readonly")
+edadP.grid(row=2, column=1, sticky="w", pady=5, padx=5)
 
 #genero
 labelgenero=tk.Label(frame_pacientes, text="genero")
@@ -70,5 +99,121 @@ centro_medico = tk.StringVar()
 centro_medico.set("Hospital Central")
 comboCentroMedico = ttk.Combobox(frame_pacientes, values=["Hospital Central", "Clinica Norte", "Centro sur"], textvariable= centro_medico)
 comboCentroMedico.grid(row=7, column=1, sticky="w", padx=5, pady=5)
+
+#frame para los botones
+btn_frame = tk.Frame(frame_pacientes)
+btn_frame.grid(row=8, column=0, columnspan=2, pady=5, sticky="w")
+ 
+#boton registrar
+btn_registrar=tk.Button(btn_frame, text="Registrar", command="", bg="lightblue")
+btn_registrar.grid(row=0,column=0, padx=5)
+ 
+#boton Elimnar
+btn_eliminar = tk.Button(btn_frame, text="Eliminar", command="", bg="red")
+btn_eliminar.grid(row=0, column=1, padx=5)
+
+#crear treeview para mostrar pacientes
+treeview = ttk.Treeview(frame_pacientes, columns=("Nombre", "FechaN", "Edad", "Genero", "GrupoS", "TipoS", "CentroM"), show="headings")
+ 
+#definir encabezados
+treeview.heading("Nombre", text="Nombre Completo")
+treeview.heading("FechaN", text="Fecha de Nacimiento")
+treeview.heading("Edad", text="Edad")
+treeview.heading("Genero", text="Genero")
+treeview.heading("GrupoS", text="Grupo sanguineo")
+treeview.heading("TipoS", text="Tipo de Seguro")
+treeview.heading("CentroM", text="Centro Medico")
+
+#definir hancho de columnas
+treeview.column("Nombre", width=120) 
+treeview.column("FechaN", width=120) 
+treeview.column("Edad", width=50, anchor="center") 
+treeview.column("Genero", width=60, anchor="center") 
+treeview.column("GrupoS", width=100, anchor="center") 
+treeview.column("TipoS", width=100, anchor="center") 
+treeview.column("CentroM", width=120)
+
+#ubicar el Treeview en la cuadricula
+treeview.grid(row=7, column=0, columnspan=2, padx=5, pady=10, sticky="nsew")
+
+#scrollbar vertical
+scroll_y=ttk.Scrollbar(frame_pacientes, orient="vertical", command=treeview.yview)
+scroll_y.grid(row=10, column=2, sticky="ns")
+
+#doctores
+#titulo
+labelTitulo=tk.Label(frame_doctores, text="Registro de Doctores")
+labelTitulo.grid(row=0, column=0, sticky="w", pady=10, padx=120)
+
+#nombre
+labelNombre=tk.Label(frame_doctores, text="Nombre Completo:")
+labelNombre.grid(row=1, column=0, sticky="w", pady=10, padx=5)
+nombreP=tk.Entry(frame_doctores)
+nombreP.grid(row=1, column=1, sticky="w", pady=10, padx=5)
+
+#especialidad
+labelEspecialidad=tk.Label(frame_doctores, text="Especialidad:")
+labelEspecialidad.grid(row=2, column=0, sticky="w", padx=5, pady=10)
+especialidad=tk.StringVar()
+especialidad.set("Neurologia")
+comboEspecialidad=ttk.Combobox(frame_doctores, values=["Neurologia","Cardiologia","Pediatria", "Traumatologia"], textvariable=especialidad)
+comboEspecialidad.grid(row=2, column=1, sticky="w", padx=5, pady=10)
+
+#edad
+labelEdad=tk.Label(frame_doctores, text="Edad:")
+labelEdad.grid(row=3, column=0, padx=5, pady=10, sticky="w")
+spinEdad=ttk.Spinbox(frame_doctores, from_=0, to=99)
+spinEdad.grid(row=3, column=1, padx=5, pady=10, sticky="w")
+
+#telefono
+labelTelefono=tk.Label(frame_doctores, text="Telefono:")
+labelTelefono.grid(row=4, column=0, sticky="w", pady=10, padx=5)
+telefonoP=tk.Entry(frame_doctores)
+telefonoP.grid(row=4, column=1, sticky="w", pady=10, padx=5)
+
+#frame para botones 
+btn_frame=tk.Frame(frame_doctores)
+btn_frame.grid(row=5, column=0, columnspan=2, pady=10, sticky="w")
+
+#boton para registro
+btn_registrar=tk.Button(btn_frame, text="Registrar", command="", bg="green", fg="white")
+btn_registrar.grid(row=5, column=0, padx=5)
+
+#boton eliminar
+btn_eliminar=tk.Button(btn_frame, text="Eliminar", command="", bg="red", fg="white")
+btn_eliminar.grid(row=5, column=1, padx=5)
+
+#crear teeview para doctores
+treeview=ttk.Treeview(frame_doctores,columns=("Nombre", "Especialidad", "Edad", "Telefono"), show="headings")
+treeview.heading("Nombre", text="Nombre Completo")
+treeview.heading("Especialidad", text="Especialidad")
+treeview.heading("Edad", text="Edad")
+treeview.heading("Telefono", text="Telefono")
+
+#definir hanchos de columnas
+treeview.column("Nombre", width=120) 
+treeview.column("Especialidad", width=120) 
+treeview.column("Edad", width=50, anchor="center") 
+treeview.column("Telefono", width=60, anchor="center") 
+treeview.grid(row=6, column=0, columnspan=2, padx=5, pady=10, sticky="nsew")
+
+#scrolbar vertical
+scroll_y=ttk.Scrollbar(frame_doctores, orient="vertical", command=treeview.yview)
+scroll_y.grid(row=6, column=2, sticky="nsew")
+
+#funcion para enmascarar
+def enmascarar_fecha(texto):
+    limpio=''.join(filter(str.isdigit, texto))
+    formato_final=""
+    if len(limpio)>8:
+        limpio=limpio[:8]
+    if len(limpio)>4:
+        formato_final=f"{limpio[:2]}-{limpio[2:4]}-{limpio[4:]}"
+    elif len(limpio)>2:
+        formato_final=f"{limpio[:2]}-{limpio[2:]}"
+    else:
+        formato_final=limpio
+
+
 
 ventana_principal.mainloop()
